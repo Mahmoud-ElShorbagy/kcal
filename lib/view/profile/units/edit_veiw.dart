69734a89c080364c83ef2d404c17adae7f6c0436
MahmoudHmaza99 <mahmhamza199@gmail.com>
2025-04-20 19:32:06 +0200

    create page edit name and image profile

---
+++ lib/view/profile/units/edit_veiw.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:kcal/auth/pick_photo/cubit.dart';
+import 'package:kcal/auth/register/cubit.dart';
+import 'package:kcal/core/helpers/app_colors.dart';
+import 'package:kcal/core/helpers/utils.dart';
+import 'package:kcal/core/helpers/validators.dart';
+import 'package:kcal/core/route_utils/route_names.dart';
+import 'package:kcal/view/image_user/view.dart';
+import 'package:kcal/view/navigate_to_page/view.dart';
+import 'package:kcal/widgets/alerting_loading.dart';
+import 'package:kcal/widgets/app/app_snack_bar.dart';
+import 'package:kcal/widgets/change_upload_image.dart';
+import 'package:kcal/widgets/custom_app_bar.dart';
+import 'package:kcal/widgets/custom_button.dart';
+import 'package:kcal/widgets/custom_text_form.dart';
+import 'package:kcal/widgets/title_text_field.dart';
+
+class EditProfileView extends StatelessWidget {
+  const EditProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    bool absorbing = false;
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => RegisterCubit(),
+        ),
+        BlocProvider(
+          create: (context) => ImagePickerCubit(),
+        ),
+      ],
+      child: BlocBuilder<RegisterCubit, RegisterState>(
+        builder: (context, state) {
+          final registerCubit = RegisterCubit.get(context);
+          return BlocBuilder<ImagePickerCubit, ImagePickerState>(
+            builder: (context, state) {
+              final imagePickerCubit = context.read<ImagePickerCubit>();
+              return Scaffold(
+                body: AbsorbPointer(
+                  absorbing: absorbing,
+                  child: SafeArea(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 16.h,
+                        ),
+                        CustomAppBar(
+                          title: "Edit Profile",
+                          tapBack: () => Navigator.pop(context),
+                        ),
+                        Expanded(
+                            child: ListView(
+                          padding: EdgeInsets.symmetric(horizontal: 25.w),
+                          children: [
+                            SizedBox(
+                              height: 48.h,
+                            ),
+                            ImageUserView(
+                              imagePath: Utils.imagePath,
+                            ),
+                            NavigateToPage.navigate(context, () {
+                              changeUplaodImage(context, () async {
+                                imagePickerCubit.pickImage(ImageSource.camera);
+                              }, () async {
+                                imagePickerCubit.pickImage(ImageSource.gallery);
+                              });
+                            }, EdgeInsets.only(top: 16.h), "Edit Photo",
+                                Alignment.center),
+                            SizedBox(height: 24.h),
+                            const TitleTextField(
+                              text: "Edit Name",
+                            ),
+                            Container(
+                                margin: EdgeInsets.only(
+                                  top: 8.h,
+                                ),
+                                child: Form(
+                                  autovalidateMode: AutovalidateMode.always,
+                                  key: registerCubit.formKey,
+                                  child: CustomTextForm(
+                                    onChanged: (v) {
+                                      registerCubit.updateValidateName(v);
+                                    },
+                                    controller: registerCubit.nameController,
+                                    validator: (v) =>
+                                        Validators.validateName(v!),
+                                    fontWeight: FontWeight.w400,
+                                    hintText: "User Name",
+                                    image: Utils.getAssetPNGPath("user"),
+                                  ),
+                                )),
+                            SizedBox(
+                              height: 145.h,
+                            ),
+                            CustomButton(
+                              color:
+                                  registerCubit.nameController.text.length >= 2
+                                      ? AppColors.primary
+                                      : AppColors.subTitle100,
+                              text: "Save",
+                              onTap: () async {
+                                if (registerCubit.formKey.currentState!
+                                    .validate()) {
+                                  absorbing = true;
+                                  registerCubit.saveName();
+
+                                  AlertingLoading.showLoading(context);
+                                  await Future.delayed(
+                                      const Duration(seconds: 3));
+                                  appSnackBar(context, "Edit Profile Success",
+                                      AppColors.primaryBaseGreen);
+                                  Navigator.pushNamed(context,
+                                      RouteNames.appBottomNavigationBar);
+                                }
+                              },
+                            ),
+                          ],
+                        )),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
