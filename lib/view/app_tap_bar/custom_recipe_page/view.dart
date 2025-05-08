8e2c9ddbf1e6bf00e0f2ff93551a7b44dbcf6302
MahmoudHmaza99 <mahmhamza199@gmail.com>
2025-05-08 09:36:01 +0300

    add widget material

--- lib/view/app_tap_bar/custom_recipe_page/view.dart
+++ lib/view/app_tap_bar/custom_recipe_page/view.dart
@@ -26,75 +26,79 @@ class CustomRecipesPageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      highlightColor: AppColors.textPrimary500.withValues(alpha: 0.2),
-      borderRadius: BorderRadius.circular(24).r,
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              CustomImageWidget(
-                image: Utils.getAssetPNGPath(imageFood),
-                width: 327.w,
-                height: 150.h,
-              ),
-              Positioned(
-                right: 20.w,
-                top: 20.h,
-                child: CustomImageWidget(
-                  image: Utils.getAssetPNGPath("Icon_heart"),
-                  width: 24.w,
-                  height: 24.h,
+    return Material(
+      child: InkWell(
+        onTap: onTap,
+        highlightColor: AppColors.textPrimary500.withValues(alpha: 0.2),
+        borderRadius: BorderRadius.circular(24).r,
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                CustomImageWidget(
+                  image: Utils.getAssetPNGPath(imageFood),
+                  width: 327.w,
+                  height: 150.h,
                 ),
-              ),
-            ],
-          ),
-          Ink(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            width: 327.w,
-            height: 120.h,
-            decoration: BoxDecoration(
-                borderRadius:
-                    const BorderRadius.vertical(bottom: Radius.circular(24)).r,
-                color: AppColors.accentYellowLight100),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              SizedBox(
-                height: 16.h,
-              ),
-              buildRowDetailsRecipes(textTime, serve),
-              SizedBox(
-                height: 8.h,
-              ),
-              Container(
-                margin: EdgeInsets.only(right: 40.w),
-                child: CustomText(
-                  letterSpacing: 0.36.sp,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w500,
-                  fontFamily: "WorkSans",
-                  text: title,
-                  color: AppColors.title,
+                Positioned(
+                  right: 20.w,
+                  top: 20.h,
+                  child: CustomImageWidget(
+                    image: Utils.getAssetPNGPath("Icon_heart"),
+                    width: 24.w,
+                    height: 24.h,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 4.h,
-              ),
-              CustomText(
-                letterSpacing: 0.36.sp,
-                fontSize: 14.sp,
-                fontWeight: FontWeight.w500,
-                fontFamily: "WorkSans",
-                text: content,
-                color: AppColors.secondaryText,
-              ),
-            ]),
-          ),
-          SizedBox(
-            height: 13.w,
-          )
-        ],
+              ],
+            ),
+            Ink(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              width: 327.w,
+              height: 120.h,
+              decoration: BoxDecoration(
+                  borderRadius:
+                      const BorderRadius.vertical(bottom: Radius.circular(24))
+                          .r,
+                  color: AppColors.accentYellowLight100),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 16.h,
+                    ),
+                    buildRowDetailsRecipes(textTime, serve),
+                    SizedBox(
+                      height: 8.h,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 40.w),
+                      child: CustomText(
+                        letterSpacing: 0.36.sp,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: "WorkSans",
+                        text: title,
+                        color: AppColors.title,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4.h,
+                    ),
+                    CustomText(
+                      letterSpacing: 0.36.sp,
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: "WorkSans",
+                      text: content,
+                      color: AppColors.secondaryText,
+                    ),
+                  ]),
+            ),
+            SizedBox(
+              height: 13.w,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/app/custom_product_list.dart
+++ lib/widgets/app/custom_product_list.dart
@@ -21,35 +21,37 @@ class CutsomProductList extends StatelessWidget {
     return Column(
       children: [
         SizedBox(height: 16.h),
-        InkWell(
-          highlightColor: AppColors.textPrimary500.withValues(alpha: 0.2),
-          borderRadius: BorderRadius.circular(16).r,
-          onTap: onTap,
-          child: Ink(
-            width: 152.w,
-            height: 72.h,
-            decoration: BoxDecoration(
-              color: AppColors.accentYellowLight100,
-              borderRadius: BorderRadius.circular(16).r,
-            ),
-            child: Row(
-              children: [
-                CustomImageWidget(image: image, width: 40.w, height: 40.h),
-                SizedBox(width: 8.w),
-                CustomText(
-                  letterSpacing: 0.4.sp,
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.w500,
-                  fontFamily: "WorkSans",
-                  text: text,
-                  color: AppColors.accentYellowDark900,
-                ),
-                const Spacer(),
-                CustomImageWidget(
-                    image: Utils.getAssetPNGPath("foaward"),
-                    width: 24.w,
-                    height: 24.h),
-              ],
+        Material(
+          child: InkWell(
+            highlightColor: AppColors.textPrimary500.withValues(alpha: 0.2),
+            borderRadius: BorderRadius.circular(16).r,
+            onTap: onTap,
+            child: Ink(
+              width: 152.w,
+              height: 72.h,
+              decoration: BoxDecoration(
+                color: AppColors.accentYellowLight100,
+                borderRadius: BorderRadius.circular(16).r,
+              ),
+              child: Row(
+                children: [
+                  CustomImageWidget(image: image, width: 40.w, height: 40.h),
+                  SizedBox(width: 8.w),
+                  CustomText(
+                    letterSpacing: 0.4.sp,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: "WorkSans",
+                    text: text,
+                    color: AppColors.accentYellowDark900,
+                  ),
+                  const Spacer(),
+                  CustomImageWidget(
+                      image: Utils.getAssetPNGPath("foaward"),
+                      width: 24.w,
+                      height: 24.h),
+                ],
+              ),
             ),
           ),
         ),
